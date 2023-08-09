786ce198570d8d6b27585530ab113cfe921bce6f
John Blesson-Rowe <106766947+Johnblesson@users.noreply.github.com>
2023-08-09 04:10:21 +0000

    update contact form

---
+++ contact.js
@@ -0,0 +1,59 @@
+// // Your web app's Firebase configuration
+var config = {
+    apiKey: "AIzaSyCEaPnPHV8yEnF2MUrJE6R6Knd9DXCVqf0",
+    authDomain: "sierrachurch.firebaseapp.com",
+    databaseURL: "https://sierrachurch-default-rtdb.firebaseio.com",
+    projectId: "sierrachurch",
+    storageBucket: "1060162593016",
+    messagingSenderId: "1:1060162593016:web:3c42a18bf3d9f7021e5b0d"
+  };
+  firebase.initializeApp(config);
+  
+  // Reference messages collection
+  var messagesRef = firebase.database().ref('messages');
+  
+  // Listen for form submit
+  document.getElementById('contactForm').addEventListener('submit', submitForm);
+  
+  // Submit form
+  function submitForm(e){
+    e.preventDefault();
+  
+    // Get values
+    var name = getInputVal('name');
+    var company = getInputVal('company');
+    var email = getInputVal('email');
+    var phone = getInputVal('phone');
+    var message = getInputVal('message');
+  
+    // Save message
+    saveMessage(name, company, email, phone, message);
+  
+    // Show alert
+    document.querySelector('.alert').style.display = 'block';
+  
+    // Hide alert after 3 seconds
+    setTimeout(function(){
+      document.querySelector('.alert').style.display = 'none';
+    },3000);
+  
+    // Clear form
+    document.getElementById('contactForm').reset();
+  }
+  
+  // Function to get get form values
+  function getInputVal(id){
+    return document.getElementById(id).value;
+  }
+  
+  // Save message to firebase
+  function saveMessage(name, company, email, phone, message){
+    var newMessageRef = messagesRef.push();
+    newMessageRef.set({
+      name: name,
+      company:company,
+      email:email,
+      phone:phone,
+      message:message
+    });
+  }
